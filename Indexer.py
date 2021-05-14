f19f9db2cbfcbba63b02bdfd44b4f5fd949789a4
Kevin <kevinhuynh914@gmail.com>
2021-05-13 18:23:10 -0700

    milestone 2

--- Indexer.py
+++ Indexer.py
@@ -1,18 +1,40 @@
 from bs4 import BeautifulSoup
 import re
 from urllib.parse import urldefrag
+import math
 
 #Inverted Index Template
 # {
 #     token: {
 #         total_frequency: 0,
-#         postings: [{url_id: token_freq}],
+#         postings: {url_id: TD-IDF},
 #         tf-idf: 0 (total_frequency for now)
 #     
 # }
 
 class Indexer:
     invertedIndex = {}
+    total_documents = 0 
+    stop_words = ["", "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't",
+                "as", "at", "be", "because", "been",
+                "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't",
+                "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for",
+                "from", "further", "had", "hadn't", "has", "hasn't",
+                "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers",
+                "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll",
+                "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's",
+                "me", "more", "most", "mustn't", "my",
+                "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our",
+                "ours", "ourselves", "out",
+                "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't",
+                "so", "some", "such", "than", "that", "that's", "the", "their",
+                "theirs", "them", "themselves", "then", "there",
+                "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to",
+                "too",
+                "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were",
+                "weren't", "what", "what's", "when", "when's", "where", "where's", "which",
+                "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you",
+                "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
 
     def __init__(self):
         pass
@@ -22,12 +44,24 @@ class Indexer:
         soup = BeautifulSoup(content, 'html.parser')
         tokens = re.findall(r'[A-Za-z0-9]+', soup.get_text().lower())
         for t in tokens:
-            if t not in self.invertedIndex:
-                self.invertedIndex[t] = {"tf-idf": 1, "postings": {defraggedUrl: 1}}
-            else:
-                self.invertedIndex[t]["tf-idf"] += 1
-                if defraggedUrl not in self.invertedIndex[t]['postings']:
-                    self.invertedIndex[t]['postings'][defraggedUrl] = 1
+
+            if t not in self.stop_words: 
+                if t not in self.invertedIndex:
+                    self.invertedIndex[t] = {"total_frequency": 1, "postings": {defraggedUrl: 1}}
                 else:
-                    self.invertedIndex[t]['postings'][defraggedUrl] += 1
+                    self.invertedIndex[t]["total_frequency"] += 1 
+                    if defraggedUrl not in self.invertedIndex[t]['postings']:
+                        self.invertedIndex[t]['postings'][defraggedUrl] = 1
+                    else:
+                        self.invertedIndex[t]['postings'][defraggedUrl] += 1
+        self.total_documents += 1
+
+    def compute_tdidf(self):
+        for token, value in self.invertedIndex.items():
+            for p,freq in self.invertedIndex[token]["postings"].items():
+                total_freq = freq
+
+                self.invertedIndex[token]["postings"][p] = total_freq * math.log(self.total_documents/len(self.invertedIndex[token]["postings"])) 
+
+            self.invertedIndex[token]["postings"]
             
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     indexer = Indexer()
     numDocs = 0
 
-    for subdir, dirs, files in os.walk(r'C:\Users\Justin Ho\Documents\CS 121\developer\DEV'): ## TODO: need to update to directory's DEV folder
+    for subdir, dirs, files in os.walk(r'C:\Users\Kevin Huynh\Projects\cs121-a3\DEV'): ## TODO: need to update to directory's DEV folder
         for filename in files:
             filepath = subdir + os.sep + filename
             f = open(filepath)
@@ -17,13 +17,103 @@ def main():
             indexer.parse(data['content'], data['url'])
             numDocs += 1
     
-    sortedTokens = sorted(indexer.invertedIndex.items(), key=lambda x: x[1]["tf-idf"], reverse=True)
+    indexer.compute_tdidf()
+    sortedTokens = sorted(indexer.invertedIndex.items(), key=lambda x: x[1]["total_frequency"], reverse=True)
     print("Number of Documents: {}".format(numDocs))
     print("Number of Unique Tokens: {}".format(len(indexer.invertedIndex.keys())))
     file1 = open("index.txt", "a")
     for k,v in sortedTokens:
-        file1.write("{}:{}\n".format(k,v))
+        postings = v["postings"]
+        sorted_postings = sorted(postings.items(), key=lambda x: x[1], reverse=True)
+        
+        file1.write("{}:{}\n".format(k,sorted_postings))
     file1.close()
 
 
+def intersection(lst1, lst2):
+    return list(set(lst1) & set(lst2))
+
+def milestone2():
+
+
+    running = True
+
+ 
+
+        
+
+    # words = ["cristina lopes", "machine learning", "ACM",  "master of software engineering"]
+    
+
+    while(running):
+        query = input("Search... ")
+
+        words = query.split()
+        queries = dict()
+
+        num_of_words = len(words)
+
+    
+        file1 = open("index.txt", "r")  
+
+        found_words = 0
+        for line in file1:
+            
+            if found_words == num_of_words:
+                break
+
+            token = line.split(":[")[0]
+
+            if token in words:
+                found_words += 1
+
+                new_line = line.split(":[")[1].replace("]","")
+                postings = new_line.split("), (")
+
+                queries[token] = {}
+                for doc in postings: 
+                    mapping = doc.strip().split(",")
+                    url = mapping[0].replace("(","").replace("'","")
+                    score = float(mapping[1].replace(")",""))
+                    
+                    
+                    queries[token][url] = score
+
+
+        AND_postings = []
+
+
+        for token in queries.keys():
+            docs = queries[token].keys()
+
+            if len(AND_postings) == 0:
+                AND_postings = docs
+            else:
+                AND_postings = intersection(AND_postings, docs)
+
+
+
+        total_doc_scores = dict()
+
+        for docs in AND_postings:
+            score = 0
+            for token in queries.keys():
+                score += queries[token][docs]
+
+            total_doc_scores[docs] = score
+
+
+        top5 = sorted(total_doc_scores.items(), key=lambda x: x[1], reverse=True)
+
+        print(top5)
+
+            
+
+
+            
+
+
+    
+
 main()
+# milestone2()
